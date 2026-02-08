0e1943ac76d0eb0770dc7fbbfa78ce43d7451193
Shahd Al Harthy <3075712a@student.gla.ac.uk>
2026-02-08 21:52:07 +0000

    Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -10,24 +10,18 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only -- or all if less than 5.
-    # Place the list in our context_dict dictionary (with our boldmessage!)
-    # that will be passed to the template engine.
+
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-
+    context_dict = {}
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    
-
-    # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict = {'boldmessage': 'Hadeel Albalushi'}
+    return render(request, 'rango/about.html', context=context_dict)
 
 def add_category(request):
     form = CategoryForm()
@@ -48,6 +42,7 @@ def add_category(request):
     # Will handle the bad form, new form, or no form supplied cases.
     # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
+
 
 def add_page(request, category_name_slug):
     try:
